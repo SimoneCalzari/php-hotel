a411e86893ef4c19adde6580952de1c2d6685cb6
SimoneCalzari <simone.calzari@outlook.it>
2024-01-09 17:04:59 +0100

    feat: bonus 1

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+// variabile per filtro parcheggio
+$parcheggio_si_no = $_GET['parking'] ?? '1';
 
 $hotels = [
 
@@ -55,42 +57,78 @@ $hotels = [
 
 
 <body>
-  <div class="container-fluid">
-    <table class="table table-hover ">
-      <thead class="table-info">
-        <tr>
-          <th scope="col" class="col-3">Nome</th>
-          <th scope="col" class="col-3">Descrizione</th>
-          <th scope="col" class="col-2">Parcheggio</th>
-          <th scope="col" class="col-2">Voto</th>
-          <th scope="col" class="col-2">Distanza dal centro</th>
-        </tr>
-      </thead>
-      <tbody class="table-light table-group-divider">
-        <?php
-        foreach ($hotels as $index => $hotel) {
-          // apro tag riga
-          $table_row = '<tr>';
-          foreach ($hotel as $key => $value) {
-            // concatento i vari tag td
-            //condizioni per mostrare yes or no nei valori booleani
-            if (is_bool($value)) {
-              $parking_str = $value ? 'Si' : 'No';
-              $table_row .= "<td>$parking_str</td>";
-            } else {
-              $table_row .= "<td>$value</td>";
-            }
-          }
-          // chiudo tag td
-          $table_row .= '</tr>';
-          // mando nell html la riga creata
-          echo $table_row;
-        }
-        ?>
-      </tbody>
-    </table>
+  <!-- HEADER -->
+  <header>
+    <h1 class="text-uppercase text-center">Php hotels</h1>
+  </header>
+  <!-- /HEADER -->
+  <!-- MAIN -->
+  <main>
+    <!-- FORM FILTRI-->
+    <section class="mb-4">
+      <div class="container-fluid">
+        <form action="index.php" method="GET">
+          <label for="parking" class="mb-1">Disponibilità parcheggio:</label>
+          <select class="form-select mb-3" id="parking" name="parking">
+            <option value="1">Indifferente</option>
+            <option value="2">Si</option>
+            <option value="3">No</option>
+          </select>
+          <button class="btn btn-primary">Filtra</button>
+        </form>
+      </div>
+    </section>
+    <!-- /FORM FILTRI-->
+    <!-- TABELLA HOTELS -->
+    <section>
+      <div class="container-fluid">
+        <table class="table table-hover">
+          <!-- HEADER TABELLA -->
+          <thead class="table-info">
+            <tr>
+              <th scope="col" class="col-3">Nome</th>
+              <th scope="col" class="col-3">Descrizione</th>
+              <th scope="col" class="col-2">Parcheggio</th>
+              <th scope="col" class="col-2">Voto</th>
+              <th scope="col" class="col-2">Distanza dal centro</th>
+            </tr>
+          </thead>
+          <!-- /HEADER TABELLA -->
+          <!-- BODY TABELLA -->
+          <tbody class="table-light table-group-divider">
+            <?php
+            foreach ($hotels as $index => $hotel) {
+              // condizioni filtro parcheggio
+              if ($parcheggio_si_no === '1' || ($parcheggio_si_no === '2' && $hotel['parking'] === true) || ($parcheggio_si_no === '3' && $hotel['parking'] === false)) {
 
-  </div>
+                // apro tag riga
+                $table_row = '<tr>';
+                foreach ($hotel as $key => $value) {
+                  // concatento i vari tag td
+                  //condizioni per mostrare yes or no nei valori booleani
+                  if (is_bool($value)) {
+                    $parking_str = $value ? 'Si' : 'No';
+                    $table_row .= "<td>$parking_str</td>";
+                  } else {
+                    $table_row .= "<td>$value</td>";
+                  }
+                }
+                // chiudo tag td
+                $table_row .= '</tr>';
+                // mando nell html la riga creata
+                echo $table_row;
+              }
+            }
+            ?>
+          </tbody>
+          <!-- /BODY TABELLA -->
+        </table>
+      </div>
+    </section>
+    <!-- /TABELLA HOTELS -->
+  </main>
+  <!-- /MAIN -->
+
 </body>
 
 </html>
